62cd11a2185e732cf3c8538451ed4836e334092b
Mustafif <mustafif0929@gmail.com>
2024-06-08 17:45:08 -0400

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use std::collections::binary_heap::Iter;
 use std::path::PathBuf;
 use std::sync::Arc;
 
 use anyhow::Result;
 use futures::future::join_all;
+use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use reqwest::Client;
 use sled::Db;
 use tokio::fs::create_dir;
@@ -12,15 +14,14 @@ use url::Url;
 /// Entry trait that will be used to check if it has been modified
 /// so `db` can compare and swap, and also return the `url`.
 pub trait Entry{
-    fn is_modified(&self) -> bool;
+    fn is_modified(&self, other: &Self) -> bool;
     fn url(&self) -> String;
-
     fn entry_bytes(&self) -> &[u8];
 }
 
 impl Entry for String{
-    fn is_modified(&self) -> bool {
-        false
+    fn is_modified(&self, _other: &String) -> bool {
+        false // urls aren't modified
     }
 
     fn url(&self) -> String {
@@ -39,6 +40,30 @@ pub struct Fetcher<E: Entry> {
     client: Client,
 }
 
+#[derive(Debug, Clone)]
+pub struct EntryIterator<E: Entry>{
+    inner: std::vec::IntoIter<E>
+}
+
+impl <E: Entry> Iterator for EntryIterator<E>{
+    type Item = E;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next()
+    }
+}
+
+impl<E: Entry> IntoIterator for Fetcher<E> {
+    type Item = E;
+    type IntoIter = EntryIterator<E>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        EntryIterator {
+            inner: self.entries.into_iter()
+        }
+    }
+}
+
 impl<E: Entry + Clone + Send + Sync + 'static + Iterator> Fetcher<E> {
     /// Create a new `Fetcher` instance with list of urls and cache db name
     pub fn new(urls: &Vec<E>, name: &str) -> Result<Self> {
@@ -53,12 +78,17 @@ impl<E: Entry + Clone + Send + Sync + 'static + Iterator> Fetcher<E> {
         })
     }
 
+    pub fn iter(&self) -> EntryIterator<E>{
+        EntryIterator{
+            inner: self.entries.clone().into_iter()
+        }
+    }
+
     pub fn set_client(&mut self, client: Client) {
         self.client = client;
     }
 
     // TODO: switch from `println!` to logging `info`
-    // TODO: switch from inserting `url.bytes` to `entry.as_bytes()`
     async fn fetch_entry(&mut self, entry: E) -> Result<()> {
         if self.db.get(&entry.url())?.is_some() {
             // add some logic to compare and swap
@@ -77,7 +107,7 @@ impl<E: Entry + Clone + Send + Sync + 'static + Iterator> Fetcher<E> {
         let mut tasks = Vec::new();
         let fetcher = Arc::new(RwLock::new(self.clone()));
 
-        for entry in self.entries.clone() {
+        for entry in self.iter().into_par_iter() {
             let fetcher = fetcher.clone();
             tasks.push(tokio::spawn(async move {
                 let mut fetcher = fetcher.write().await;
